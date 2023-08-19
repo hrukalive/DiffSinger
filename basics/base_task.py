e87b8dcf9383dac5ccd0bc486085781e7ccc2d10
hrukalive <bowen.parallife@gmail.com>
2023-08-18 21:07:33 -0500

    Try to optimize for TPU

--- basics/base_task.py
+++ basics/base_task.py
@@ -77,6 +77,7 @@ class BaseTask(pl.LightningModule):
         self.model = None
         self.skip_immediate_validation = False
         self.skip_immediate_ckpt_save = False
+        self.use_tpu = isinstance(self.trainer.accelerator, pl.accelerators.XLAAccelerator)
 
         self.valid_losses: Dict[str, Metric] = {
             'total_loss': MeanMetric()
@@ -360,6 +361,7 @@ class BaseTask(pl.LightningModule):
         )
 
     def val_dataloader(self):
+        use_tpu = isinstance(self.trainer.accelerator, pl.accelerators.XLAAccelerator)
         sampler = DsEvalBatchSampler(
             self.valid_dataset,
             max_batch_frames=self.max_val_batch_frames,
